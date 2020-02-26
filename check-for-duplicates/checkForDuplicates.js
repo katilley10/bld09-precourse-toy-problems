42e083b840573b174d9e33ea418001fc45e98d5e
VincentCSS <VincentHTML@gmail.com>
2020-02-25 20:00:28 -0700

    check for duplicates problem

---
+++ check-for-duplicates/checkForDuplicates.js
@@ -0,0 +1,11 @@
+/*
+  Write a function that checks if an array contains any duplicates values
+  Return false if there are no duplicates, true if there are.
+   
+  checkForDuplicates([1, 2, 3, 4]) => returns false
+  checkForDuplicates([1, 3, 4, 1]) => returns true
+*/
+
+const checkForDuplicates = (array) => {
+
+};
