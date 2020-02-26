c1b7d05a987a2dbdfd58ef0a694f1c303e891385
VincentCSS <VincentHTML@gmail.com>
2020-02-25 23:14:12 -0700

    added examples

--- check-for-duplicates/checkForDuplicates.js
+++ check-for-duplicates/checkForDuplicates.js
@@ -1,9 +1,11 @@
 /*
-  Write a function that checks if an array contains any duplicates values
+  Write a function that given an array checks if there are any duplicates values
   Return false if there are no duplicates, true if there are.
-   
+
   checkForDuplicates([1, 2, 3, 4]) => returns false
   checkForDuplicates([1, 3, 4, 1]) => returns true
+  checkForDuplicates(['yes', 'no']) => returns false
+  checkForDuplciates(['yes', 'yes']) => returns true
 */
 
 const checkForDuplicates = (array) => {
