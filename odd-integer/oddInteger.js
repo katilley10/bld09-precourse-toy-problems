7f093c498ff564edd54dff811e5554c12c4890ad
Bailey <vincenthtml@gmail.com>
2020-03-05 16:37:01 -0700

    W2D5: oddInteger

---
+++ odd-integer/oddInteger.js
@@ -0,0 +1,22 @@
+/*
+    Write a function which when given an array
+    finds the integer that appears an odd number of times
+
+    If there are multiple integers return an array of them
+    sorted by ascending integers otherwise return the one integer.
+
+    ex: oddInteger([1, 1, 1, 2]) => [1, 2]
+    ex: oddInteger([1, 1, 2, 2, 3]) => 3
+    ex: oddInteger([5, 7, 5, 7, 3, 2, 3, 3]) => [2, 3]
+    ex: oddInteger([5, 4, 3, 2, 1]) => [1, 2, 3, 4, 5]
+*/
+
+const oddInteger = (array) => {
+
+};
+
+//Do not change this line or the function name
+module.exports = oddInteger;
+
+//Run this line to check your answer
+//npm test ./odd-integer/oddInteger.test.js
