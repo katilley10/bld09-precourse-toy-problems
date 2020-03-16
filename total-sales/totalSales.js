7d9243ba6f1257f3a7f407d02594c8b08590dfe5
kyle tilley <katilley10@gmail.com>
2020-03-16 11:14:32 -0600

    solved

--- total-sales/totalSales.js
+++ total-sales/totalSales.js
@@ -45,8 +45,19 @@ const salesTeam = {
 };
 
 const totalSales = (team) => {
-
-};
+    if (team === undefined) {
+      return 0;
+    } else if (Array.isArray(team) == false && team.manages.length === 0) {
+      return team.individualSales;
+    } else if (Array.isArray(team) == false && team.manages.length > 0) {
+      return team.individualSales + totalSales(team.manages);
+    } else if (Array.isArray(team) && team.length > 1) {
+      return totalSales(team[0]) + totalSales(team.slice(1));
+    } else if (Array.isArray(team) && team.length === 1) {
+      return totalSales(team[0]);
+    }
+  };
+  
 
 //Do not change this line, the function name, or salesTeam object!
 module.exports = { totalSales, salesTeam };
